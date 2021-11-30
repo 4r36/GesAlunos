3e57d9f5d71d867a46ffa07ab7326ca09222f803
4r36 <93591244+4r36@users.noreply.github.com>
2021-11-30 10:43:19 -0100

    Add files via upload

--- index.js
+++ index.js
@@ -11,13 +11,6 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./public/index.html'))
 })
 
-/*app.get('/', (req, res)=>{
-  res.send('Hello World')
-  }) 
-app.get('/:nome', (req, res)=>{
-  res.send('Hello '+ req.params.nome)
-})  */
-
 const port = 3000
  
 app.listen(port, () =>{
