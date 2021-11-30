d1903dc7a477794fa987f14f0a93a6e5220caed3
4r36 <93591244+4r36@users.noreply.github.com>
2021-11-30 10:21:10 -0100

    teste

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('./public'))
+
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
+/*app.get('/', (req, res)=>{
+  res.send('Hello World')
+  }) 
+app.get('/:nome', (req, res)=>{
+  res.send('Hello '+ req.params.nome)
+})  */
+
+const port = 3000
+ 
+app.listen(port, () =>{
+    console.log(`Listening on port ${port}`)
+})
