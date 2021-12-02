0b0bd1862f45f13a7e9b4a51be109d97a6f479d5
4r36 <rodrigocostamoura@hotmail.com>
2021-12-02 09:17:10 -0100

    ok

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'./public/index.html'))
 })
 
-const port = 3000
+const port = 5100
  
 app.listen(port, () =>{
     console.log(`Listening on port ${port}`)
